8427387312d6a4eacf295892ba3b2a78a4153275
김무성 <ddb8036631@gmail.com>
2021-08-01 15:38:39 +0900

    Update August 1, 2021

--- src/구현/Test.java
+++ src/구현/Test.java
@@ -1,17 +1,19 @@
 package 구현;
 
-import java.sql.SQLOutput;
-import java.util.Arrays;
+class User {
+    String userName;
+    int age;
+    private int id;
 
+    public User(String userName, int age, int id) {
+        this.userName = userName;
+        this.age = age;
+        this.id = id;
+    }
+}
 public class Test {
     public static void main(String[] args) {
-        String[] arr = new String[3];
-        arr[0] = "This";
-        arr[1] = "is";
-        arr[2] = "JavaScript";
-
-        Arrays.sort(arr);
-
-        System.out.println(Arrays.toString(arr));
+        User user = new User("Kim", 28, 19384029);
+        System.out.println(user.);
     }
 }
