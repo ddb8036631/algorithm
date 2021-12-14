f4c0659fdd4d312c1da292038c751f8ee34946a9
김무성 <ddb8036631@gmail.com>
2021-12-14 14:52:06 +0900

    [Python] Programmers 42840. 모의고사
    완전탐색
    
    https://programmers.co.kr/learn/courses/30/lessons/42840

--- src/프로그래머스_Level1/모의고사.py
+++ src/프로그래머스_Level1/모의고사.py
@@ -1,56 +1,27 @@
 def solution(answers):
     answer = []
-    p1 = [1, 2, 3, 4, 5]
-    p2 = [
-        2,
-        1,
-        2,
-        3,
-        2,
-        4,
-        2,
-        5,
-    ]
-    p3 = [
-        3,
-        3,
-        1,
-        1,
-        2,
-        2,
-        4,
-        4,
-        5,
-        5,
-    ]
-    c1 = 0
-    c2 = 0
-    c3 = 0
+    personOne = [1,2,3,4,5]
+    personTwo = [2,1,2,3,2,4,2,5]
+    personThree = [3,3,1,1,2,2,4,4,5,5]
+    countOne = countTwo = countThree = 0
 
-    for i in range(0, len(answers)):
-        if p1[i % len(p1)] == answers[i]:
-            c1 += 1
-        if p2[i % len(p2)] == answers[i]:
-            c2 += 1
-        if p3[i % len(p3)] == answers[i]:
-            c3 += 1
+    for i in range(len(answers)):
+        if answers[i] == personOne[i % len(personOne)]:
+            countOne += 1
+        if answers[i] == personTwo[i % len(personTwo)]:
+            countTwo += 1
+        if answers[i] == personThree[i % len(personThree)]:
+            countThree += 1
 
-    max_cnt = c1
-    max_cnt = max(max_cnt, c2)
-    max_cnt = max(max_cnt, c3)
-
-    print("max_cnt : ", max_cnt)
-    print(c1, c2, c3)
-
-    if max_cnt == c1:
-        answers.append(1)
-    if max_cnt == c2:
-        answers.append(2)
-    if max_cnt == c3:
-        answer.append(3)
-
-    return answers
+    counts = [countOne, countTwo, countThree]
+    
+    for idx, cnt in enumerate(counts):
+        if cnt == max(counts):
+            answer.append(idx + 1)
+    
+    return answer
 
 
-print(solution([1, 2, 3, 4, 5]))
-print(solution([1, 3, 2, 4, 2]))
+print(solution([1,2,3,4,5]))
+print(solution([1,3,2,4,2]))
+
